f3c8f45434e359ab5d808df45005165be60be127
arppanaar <124188911+arppanaar@users.noreply.github.com>
2023-02-18 08:37:27 +0530

    Add files via upload

---
+++ rectangle (2).py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+class Rectangle:
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
+    
+    def area(self):
+        return self.length * self.breadth
+    
+    def perimeter(self):
+        return 2 * (self.length + self.breadth)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
